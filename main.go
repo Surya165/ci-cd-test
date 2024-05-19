a55084238a6e8e189fea40089d8c7e447b790c43
Surya165 <surya.kanna@razorpay.com>
2024-05-19 13:18:50 +0530

    changed sample application

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	gin.Default().GET("/hello", func(c *gin.Context) { c.String(200, "hello, this is vinay surya") }).Run(":8080")
+	// Initialize Gin
+	r := gin.Default()
+
+	// Define route for /hello
+	r.GET("/hello", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello Surya",
+		})
+	})
+
+	// Run the server on port 8080
+	r.Run(":8080")
 }
